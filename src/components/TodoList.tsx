c45d820a36afacd48348eff23fd530469f96affc
Valeria <lerik13@gmail.com>
2021-12-13 13:45:58 -0700

    pagination in TodoList, combine all actions in hook useActions

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,14 +1,15 @@
 import React, { useEffect } from 'react';
-import { useActions } from '../hooks/useActions';
 import { useTypedSelector } from '../hooks/useTypedSelector';
+import { useActions } from '../hooks/useActions';
 
 const TodoList: React.FC = () => {
 	const {todos, page, limit, error, loading} = useTypedSelector(state => state.todo)
-	const {fetchTodos} = useActions()
+	const {fetchTodos, setTodoPage} = useActions()
+	const pages = [1,2,3,4,5]
 	
 	useEffect(() => {
 		fetchTodos(page, limit)
-	}, [])
+	}, [page])
 
 	if (loading) {
 		return <h1>Loading ...</h1>
@@ -22,6 +23,16 @@ const TodoList: React.FC = () => {
 			{todos. map(todo => 
 				<div key={todo.id}>{todo.id} - {todo.title}</div>
 			)}
+			<div style={{display: 'flex'}}>
+				{pages.map(p => 
+					<div key={p}
+						onClick={() => setTodoPage(p)}
+						style={{border: p === page ? '2px solid green' : '1px solid gray', padding: 10, cursor: 'pointer'}}
+					>
+						{p}
+					</div>
+				)}
+			</div>
 		</div>
 	);
 };
